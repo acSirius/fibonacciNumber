fa294417bef31bef4df3664f495951061fc29d98
acSirius <angelcerveraroldan765@gmail.com>
2020-08-28 16:35:43 +0100

    Finished project, finds the first 80 fibonacci numbers

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ func main () {
 	const fibonacciLength int = 80 // How long do you want the sequence to be ?
 	var fibonacciNumbers [fibonacciLength]int // Array of fibonacci numbers with the desired length
 
-	fibonacciNumbers[0], fibonacciNumbers[1] = 0, 1 // First two values of fibonacci, this need to be hard coded to be able to find the rest
+	fibonacciNumbers[0], fibonacciNumbers[1] = 0, 1 // First two values of fibonacci, these need to be hard coded to be able to find the rest
 
 	for i := 0; i < fibonacciLength;i++ {
 		if i < 2{ fmt.Println(fibonacciNumbers[i]) } else {
